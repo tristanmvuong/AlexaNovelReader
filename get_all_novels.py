add179b1be80f45881ed1d79a78df85fdf9053c4
wall <mystaticprivateemail@gmail.com>
2020-06-12 17:25:50 -0600

    generates csv for bulk adding novels to alexa skill slot type

---
+++ get_all_novels.py
@@ -0,0 +1,36 @@
+import bs4
+import urllib.request
+import urllib.error
+
+
+def main():
+    base_url = "https://boxnovel.com/novel/page/"
+    page_num = 1
+    novel_list = ''
+
+    while True:
+        req = urllib.request.Request(base_url + str(page_num), data=None, headers={'User-Agent': 'Mozilla/5.0'})
+
+        try:
+            page = urllib.request.urlopen(req)
+        except urllib.error.HTTPError as e:
+            if page_num > 1:
+                break
+            else:
+                return
+        except urllib.error.URLError as e:
+            return
+        else:
+            soup = bs4.BeautifulSoup(page, 'html.parser')
+            for item in soup.find_all('div', class_='post-title'):
+                novel_list += '"' + item.find('a').get_text() + '"' + ',\n'
+
+            page_num += 1
+
+    f = open('novels_list.csv', 'w', encoding='utf-8')
+    f.write(novel_list)
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
